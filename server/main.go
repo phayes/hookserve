fa3ed71c79a15d2efc64236184e3655cd781dd34
phayes <patrick.d.hayes@gmail.com>
2014-10-30 10:04:11 -0700

    Completing command-passing

--- server/main.go
+++ server/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/codegangsta/cli"
 	"github.com/phayes/hookserve/hookserve"
 	"os"
+	"os/exec"
 	"time"
 )
 
@@ -12,7 +13,13 @@ func main() {
 
 	app := cli.NewApp()
 	app.Name = "hookserve"
-	app.Usage = "A small little application that listens for commit / push webhook events from github and runs a specified command"
+	app.Usage = "A small little application that listens for commit / push webhook events from github and runs a specified command\n\n"
+	app.Usage += "EXAMPLE:\n"
+	app.Usage += "   hookserve --secret=whiskey --port=8888 echo  #Echo back the information provided\n"
+	app.Usage += "   hookserve logger -t PushEvent #log the push event to the system log (/var/log/message)"
+	app.Version = "1.0"
+	app.Author = "Patrick Hayes"
+	app.Email = "patrick.d.hayes@gmail.com"
 
 	app.Flags = []cli.Flag{
 		cli.IntFlag{
@@ -36,7 +43,16 @@ func main() {
 		for {
 			select {
 			case commit := <-server.Events:
-				fmt.Println(commit.Owner + " " + commit.Repo + " " + commit.Branch + " " + commit.Commit)
+				if args := c.Args(); len(args) != 0 {
+					root := args[0]
+					rest := append(args[1:], commit.Owner, commit.Repo, commit.Branch, commit.Commit)
+					cmd := exec.Command(root, rest...)
+					cmd.Stdout = os.Stdout
+					cmd.Stderr = os.Stderr
+					cmd.Run()
+				} else {
+					fmt.Println(commit.Owner + " " + commit.Repo + " " + commit.Branch + " " + commit.Commit)
+				}
 			default:
 				time.Sleep(100)
 			}
